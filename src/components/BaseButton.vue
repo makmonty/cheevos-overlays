034d3948386cbe51e9c6d8bdbc4bae833e141a8e
Ángel Brasero Moreno <angel.brasero@gmail.com>
2024-10-11 10:35:00 +0200

    Created BaseButton and BaseInput to start styling a bit the forms

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const {type = 'button'} = defineProps<{
+	type: 'button' | 'submit'
+	disabled?: boolean
+}>()
+</script>
+
+<template>
+	<button :type="type" class="button" :disabled="disabled">
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+.button {
+	border: 1px solid transparent;
+	background: var(--info-color);
+	border-radius: var(--default-border-radius);
+}
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const {type = 'text'} = defineProps<{
+	type: 'text' | 'password' | 'number' | 'email' | 'url'
+}>()
+
+const model = defineModel()
+
+</script>
+
+<template>
+	<input :type="type" v-model="model" class="input" />
+</template>
+
+<style scoped>
+.input {
+	border: 1px solid transparent;
+	border-radius: var(--default-border-radius);
+}
+</style>
