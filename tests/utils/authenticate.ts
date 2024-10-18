9cce87b12731ff743bb94be3b8a06a9443313178
Ángel Brasero Moreno <angel.brasero@gmail.com>
2024-10-18 14:19:33 +0200

    Fixing untyped parameter in authenticate

--- tests/utils/authenticate.ts
+++ tests/utils/authenticate.ts
@@ -1,6 +1,6 @@
 import { USERNAME_STORAGE_KEY, WEB_API_KEY_STORAGE_KEY } from '@/stores/auth';
 
-export const authenticate = (username: string, webApiKey) => {
+export const authenticate = (username: string, webApiKey: string) => {
   localStorage.setItem(USERNAME_STORAGE_KEY, username);
   localStorage.setItem(WEB_API_KEY_STORAGE_KEY, webApiKey);
 };
